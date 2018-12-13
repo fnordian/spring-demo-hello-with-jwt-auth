d3fa166c56e9fb43e379a388e01419caf3d5f43e
Marcus Hunger <marcus.hunger@gmail.com>
2018-12-13 21:33:41 +0100

    initial commit

---
+++ src/main/java/com/example/backendtest/hello/DemoHelloServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.backendtest.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHelloServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHelloServiceApplication.class, args);
+    }
+
+}
+
